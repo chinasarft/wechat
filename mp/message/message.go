9c3133202b2215e21783ec0dd058e1775d3d08a6
liuye <liuyeid@hotmail.com>
2017-08-01 06:54:04 +0800

    add safe mode

--- mp/message/message.go
+++ mp/message/message.go
@@ -1,14 +1,27 @@
 package message
 
 import (
+	"encoding/base64"
 	"encoding/xml"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"reflect"
+	"strconv"
 	"strings"
 	"time"
+
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha1"
+	"encoding/binary"
+	"math/big"
+	"sort"
+
+	"github.com/chinasarft/wechat/mp/token"
 )
 
 // MsgType 基本消息类型
@@ -126,19 +139,19 @@ type EventPic struct {
 }
 
 //EncryptedXMLMsg 安全模式下的消息体
-type EncryptedXMLMsg struct {
-	XMLName      struct{} `xml:"xml" json:"-"`
-	ToUserName   string   `xml:"ToUserName" json:"ToUserName"`
-	EncryptedMsg string   `xml:"Encrypt"    json:"Encrypt"`
+type EncryptMsg struct {
+	XMLName    xml.Name `xml:"xml" json:"-"`
+	ToUserName string   `xml:"ToUserName" json:"ToUserName"`
+	Encrypt    string   `xml:"Encrypt"    json:"Encrypt"`
 }
 
 //ResponseEncryptedXMLMsg 需要返回的消息体
-type ResponseEncryptedXMLMsg struct {
-	XMLName      struct{} `xml:"xml" json:"-"`
-	EncryptedMsg string   `xml:"Encrypt"      json:"Encrypt"`
-	MsgSignature string   `xml:"MsgSignature" json:"MsgSignature"`
+type ResponseEncryptMsg struct {
+	XMLName      xml.Name `xml:"xml" json:"-"`
+	Encrypt      CDATA    `xml:"Encrypt"      json:"Encrypt"`
+	MsgSignature CDATA    `xml:"MsgSignature" json:"MsgSignature"`
 	Timestamp    int64    `xml:"TimeStamp"    json:"TimeStamp"`
-	Nonce        string   `xml:"Nonce"        json:"Nonce"`
+	Nonce        CDATA    `xml:"Nonce"        json:"Nonce"`
 }
 
 // CommonToken 消息中通用的结构
@@ -148,6 +161,14 @@ type MessageHeader struct {
 	FromUserName string   `xml:"FromUserName"`
 	CreateTime   int64    `xml:"CreateTime"`
 	MsgType      MsgType  `xml:"MsgType"`
+}
+
+type CDATA string
+
+func (c CDATA) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
+	return e.EncodeElement(struct {
+		string `xml:",cdata"`
+	}{string(c)}, start)
 }
 
 func (h MessageHeader) getResponseHeader() MessageHeader {
@@ -338,26 +359,17 @@ func (this *MessageHeader) SetMsgType(msgType MsgType) {
 	this.MsgType = msgType
 }
 
-func getMixMessage(body io.ReadCloser) (*MixMessage, error) {
-	rawMsg, err := ioutil.ReadAll(body)
-	if err != nil {
-		return nil, err
-	}
-	fmt.Println("rawXml:", string(rawMsg))
+func getMixMessage(rawMsg []byte) (*MixMessage, error) {
+
 	mixMsg := &MixMessage{}
-	err = xml.Unmarshal(rawMsg, mixMsg)
+	err := xml.Unmarshal(rawMsg, mixMsg)
 	if err != nil {
 		return nil, err
 	}
 	return mixMsg, nil
 }
 
-func Handle(w http.ResponseWriter, r *http.Request) {
-	mixMsg, err := getMixMessage(r.Body)
-	if err != nil {
-		fmt.Println("1:", err)
-		return
-	}
+func handlePlain(mixMsg *MixMessage) interface{} {
 
 	mixMsgValue := reflect.ValueOf(mixMsg)
 	methodName := mixMsg.getMethodName()
@@ -368,12 +380,238 @@ func Handle(w http.ResponseWriter, r *http.Request) {
 	params[0] = request[0]
 	response := mixMsgValue.MethodByName(methodName[1]).Call(params)
 
-	respByte, err := xml.Marshal(response[0].Interface())
+	return response[0].Interface()
+}
+
+func aesDecrypt(base64Data string, aesKey []byte) ([]byte, error) {
+	cipherData, _ := base64.StdEncoding.DecodeString(base64Data)
+	k := len(aesKey) //PKCS#7
+	if len(cipherData)%k != 0 {
+		return nil, fmt.Errorf("crypto/cipher: ciphertext size is not multiple of aes key length")
+	}
+
+	block, err := aes.NewCipher(aesKey)
+	if err != nil {
+		return nil, err
+	}
+
+	iv := make([]byte, aes.BlockSize)
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return nil, err
+	}
+
+	blockMode := cipher.NewCBCDecrypter(block, iv)
+	plainData := make([]byte, len(cipherData))
+	blockMode.CryptBlocks(plainData, cipherData)
+	return plainData, nil
+}
+
+func Handle(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	timestamp := strings.Join(r.Form["timestamp"], "")
+	nonce := strings.Join(r.Form["nonce"], "")
+	//signature := strings.Join(r.Form["signature"], "")
+	encryptType := strings.Join(r.Form["encrypt_type"], "")
+	msgSignature := strings.Join(r.Form["msg_signature"], "")
+
+	var rawMsg []byte
+	var err error
+	var isSafeModel bool = false
+
+	if r.Method == http.MethodPost {
+		if encryptType == "aes" {
+			isSafeModel = true
+			fmt.Println("Wechat Service: in safe mode")
+			encryptMsg := parseEncryptRequestBody(r)
+
+			//Validate msg signature
+			if !validateMsg(timestamp, nonce, encryptMsg.Encrypt, msgSignature) {
+				fmt.Println("Wechat Service: msg_signature is invalid")
+				return
+			}
+			fmt.Println("Wechat Service: msg_signature validation is ok!")
+
+			fmt.Println("cipher:", encryptMsg.Encrypt)
+
+			aesKey := token.GetAesKey()
+			plainData, err := aesDecrypt(encryptMsg.Encrypt, aesKey)
+			if err != nil {
+				fmt.Println(len(aesKey), len(encryptMsg.Encrypt), "11:", err)
+				return
+			}
+			fmt.Println("plain:", string(plainData))
+
+			rawMsg, err = checkAndGetPlain(plainData)
+			if err != nil {
+				fmt.Println("checkAndGetPlain:", err)
+				return
+			}
+		} else {
+			rawMsg, err = ioutil.ReadAll(r.Body)
+			if err != nil {
+				fmt.Println("read err:", err)
+				return
+			}
+
+		}
+
+	} else if r.Method == http.MethodGet {
+		fmt.Println("get method")
+		return
+	} else {
+		fmt.Println(r.Method, "method")
+		return
+	}
+
+	fmt.Println("rawXml:", string(rawMsg))
+
+	mixMsg, err := getMixMessage(rawMsg)
+	if err != nil {
+		fmt.Println("1:", err)
+		return
+	}
+	result := handlePlain(mixMsg)
+
+	respByte, err := xml.MarshalIndent(result, "", "")
 	if err != nil {
 		fmt.Println("2:", err)
 		return
 	}
-	fmt.Println(methodName, string(respByte))
-	w.Write(respByte)
+	fmt.Println("plainData:", string(respByte))
+	if isSafeModel {
+		cipherRespByte, err := makeEncryptResponseXml(respByte)
+		if err != nil {
+			fmt.Printf("makeEncryptResponseXml:", err)
+			return
+		}
+		w.Header().Set("Content-Type", "text/xml")
+		fmt.Println("response:", string(cipherRespByte))
+		w.Write(cipherRespByte)
+	} else {
+		w.Write(respByte)
+	}
 	return
+}
+
+func parseEncryptRequestBody(r *http.Request) *EncryptMsg {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println("9:", err)
+		return nil
+	}
+	encryptMsg := &EncryptMsg{}
+	xml.Unmarshal(body, encryptMsg)
+	return encryptMsg
+}
+
+func validateMsg(timestamp, nonce, msgEncrypt, msgSignatureIn string) bool {
+	msgSignatureGen := makeMsgSignature(timestamp, nonce, msgEncrypt)
+	if msgSignatureGen != msgSignatureIn {
+		return false
+	}
+	return true
+}
+
+func makeMsgSignature(timestamp, nonce, msg_encrypt string) string {
+	validateToken := token.GetValidateToken()
+	sl := []string{validateToken, timestamp, nonce, msg_encrypt}
+	sort.Strings(sl)
+	s := sha1.New()
+	io.WriteString(s, strings.Join(sl, ""))
+	return fmt.Sprintf("%x", s.Sum(nil))
+}
+
+//http://studygolang.com/articles/2213
+
+func checkAndGetPlain(plainText []byte) ([]byte, error) {
+
+	// Read length
+	buf := bytes.NewBuffer(plainText[16:20])
+	var length int32
+	binary.Read(buf, binary.BigEndian, &length)
+	fmt.Println("length:", string(plainText[20:20+length]))
+
+	appId := token.GetAppId()
+	// appID validation
+	appIdstart := 20 + length
+	id := plainText[appIdstart : int(appIdstart)+len(appId)]
+	if string(id) != appId {
+		fmt.Println("Wechat Service: appid is invalid!")
+		return nil, fmt.Errorf("Appid is invalid")
+	}
+	fmt.Println("Wechat Service: appid validation is ok!")
+
+	return plainText[20 : 20+length], nil
+
+}
+
+func makeEncryptResponseXml(plainData []byte) ([]byte, error) {
+
+	// Encrypt part2: Length bytes
+	buf := new(bytes.Buffer)
+	err := binary.Write(buf, binary.BigEndian, int32(len(plainData)))
+	if err != nil {
+		fmt.Println("Binary write err:", err)
+		return nil, err
+	}
+	bodyLength := buf.Bytes()
+
+	// Encrypt part1: Random bytes
+	randomBytes := []byte("abcdefghijklmnop")
+
+	appId := token.GetAppId()
+	// Encrypt Part, with part4 - appID
+	decoratePlainData := bytes.Join([][]byte{randomBytes, bodyLength, plainData, []byte(appId)}, nil)
+	fmt.Println("decoratePlainData:", string(decoratePlainData[0:16]),
+		decoratePlainData[16:20], string(decoratePlainData[20:]))
+	cipherData, err := aesEncrypt(decoratePlainData, token.GetAesKey())
+	if err != nil {
+		return nil, fmt.Errorf("aesEncrypt error")
+	}
+
+	responseEncryptMsg := &ResponseEncryptMsg{}
+	responseEncryptMsg.Encrypt = CDATA(base64.StdEncoding.EncodeToString(cipherData))
+
+	numNonce, err := rand.Int(rand.Reader, big.NewInt(1000000000))
+	if err != nil {
+		return nil, fmt.Errorf(err.Error())
+	}
+	strNonce := numNonce.String()
+	responseEncryptMsg.Nonce = CDATA(strNonce)
+
+	responseEncryptMsg.Timestamp = time.Now().Unix()
+	sig := makeMsgSignature(
+		strconv.Itoa(int(responseEncryptMsg.Timestamp)),
+		string(responseEncryptMsg.Nonce), string(responseEncryptMsg.Encrypt))
+	responseEncryptMsg.MsgSignature = CDATA(sig)
+
+	return xml.MarshalIndent(responseEncryptMsg, "", "")
+}
+
+func aesEncrypt(plainData []byte, aesKey []byte) ([]byte, error) {
+	k := len(aesKey)
+	plainData = PKCS7Padding(plainData, k)
+
+	block, err := aes.NewCipher(aesKey)
+	if err != nil {
+		return nil, err
+	}
+
+	iv := make([]byte, aes.BlockSize)
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return nil, err
+	}
+
+	cipherData := make([]byte, len(plainData))
+	blockMode := cipher.NewCBCEncrypter(block, iv)
+	blockMode.CryptBlocks(cipherData, plainData)
+
+	return cipherData, nil
+}
+
+func PKCS7Padding(ciphertext []byte, blockSize int) []byte {
+	padding := blockSize - len(ciphertext)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(ciphertext, padtext...)
 }
